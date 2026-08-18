130af70a2e9652f9ff7776a137ece2f148186791
salwa-said <salwwa2000@gmail.com>
2026-08-18 15:26:38 +0400

    feat(entities): Person getters/setters   [Task 1.1]

--- java/entities/Person.java
+++ java/entities/Person.java
@@ -17,6 +17,42 @@ public class Person {
         this.lastName = lastName;
         this.age = age;
     }
-    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        if(id==0 && id<0){
+            System.out.println("Error: id cannot be 0");
+        }else {
+            this.id = id;
+        }
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
 
 }
